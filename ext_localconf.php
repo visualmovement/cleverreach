12ad2db0c89065a78b608bd4c6f6fbac51890768
bpaulsen-abd <bp@albertbauerdigital.com>
2020-09-18 14:03:02 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,5 +2,5 @@
 defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-	'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TypoScript/TsConfig/Page/powermail.tsconfig">'
+	'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:cleverreach/Configuration/TypoScript/TsConfig/Page/powermail.tsconfig">'
 );
